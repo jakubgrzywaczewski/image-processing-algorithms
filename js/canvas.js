947389cc1040311df1c9f57fad6715d7765a3bcc
Jakub Grzywaczewski <grzywaczewski.jakub@gmail.com>
2018-01-18 16:35:14 +0100

    Canvas is scalling properly

--- js/canvas.js
+++ js/canvas.js
@@ -5,19 +5,38 @@ const LOADED_FILE = document.querySelector('#load-file');
 LOADED_FILE.addEventListener('change', () => {
 	let path = document.querySelector('.file-path');
 	path.placeholder = LOADED_FILE.files[0].name;
-	CreateImage(LOADED_FILE.files[0]);
+	createCanvasImage(LOADED_FILE.files[0]);
 }, false);
 
-function CreateImage(file) {
+function loadImage(url) {
+	return new Promise(resolve => { 
+		let i = new Image(); 
+		i.onload = () => { resolve(i) }; 
+		i.src = url; 
+	});
+}
+
+function correctCanvasSizes(image) {
+	CANVAS.width = image.width;
+	CANVAS.height = image.height;
+	if(image.width > 1300) {
+		let body = document.querySelector('body');
+		body.innerHTML = "";
+		body.innerHTML = "<h1 style='display:block; margin: 100px auto;'>Zbyt duży obrazek.<br> Maksymalny rozmiar obrazka to 1300px</h1>";
+	}
+}
+
+async function createCanvasImage(file) {
 	let reader = new FileReader();
-	let image = new Image();
 	let url = URL.createObjectURL(file);
+	let image = await loadImage(url);
 	image.onload = function () {
-		CANVAS_CTX.drawImage(image, 0, 0, image.width, image.height, 0, 0, CANVAS.width, CANVAS.height);
-		CANVAS_CTX.fillStyle = "rgba(200, 0, 0, 0)";
+		CANVAS_CTX.drawImage(image, 0, 0, CANVAS.width, CANVAS.height, 0, 0, image.width, image.height);
+		CANVAS_CTX.fillStyle = "rgba(0, 0, 0, 0.1)";
 		CANVAS_CTX.fillRect(0, 0, CANVAS.width, CANVAS.height);
 	};
 	image.src = url;
+	correctCanvasSizes(image);
 }
 
 function getPixel(url, x, y) {
