bd9ffd10ec037f99c0e487cd90bc61759b1fffbe
Jakub Grzywaczewski <grzywaczewski.jakub@gmail.com>
2018-01-19 23:06:20 +0100

    heroku config added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var express = require("express");
-var app = express();
-var path = require("path");
+const express = require("express");
+const app = express();
+const path = require("path");
+let port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
@@ -8,4 +9,4 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));   
 });
 
-app.listen(3000, () => console.log('Example app listening on port 8080!'));
+app.listen(port));
