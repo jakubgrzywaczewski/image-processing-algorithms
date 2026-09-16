fc5c8bba158915c5dce36246bd4d1a6aa29386e4
Jakub Grzywaczewski <grzywaczewski.jakub@gmail.com>
2026-09-16 19:40:20 +0200

    Prepare codebase for TypeScript 7

--- src/types/algorithms.ts
+++ src/types/algorithms.ts
@@ -1,7 +1,9 @@
-export enum Algorithm {
-  FLOYD_STEINBERG = 'floyd-steinberg',
-  GRAYSCALE = 'grayscale',
-  REVERSE = 'reverse',
-  RESTORE = 'restore',
-  DOWNLOAD = 'download',
-}
+export const Algorithm = {
+  FLOYD_STEINBERG: 'floyd-steinberg',
+  GRAYSCALE: 'grayscale',
+  REVERSE: 'reverse',
+  RESTORE: 'restore',
+  DOWNLOAD: 'download',
+} as const;
+
+export type Algorithm = (typeof Algorithm)[keyof typeof Algorithm];
